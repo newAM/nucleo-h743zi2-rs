217d841590e32fb93257aa7a87b371a99ab28933
Alex Martens <alexmgit@protonmail.com>
2021-12-22 14:28:39 -0800

    update dependencies

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 #![no_main]
 
 use defmt_rtt as _; // global logger
-use panic_probe as _; // panic handler
+use panic_probe as _;
+use stm32h7xx_hal as _; // panic handler
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
